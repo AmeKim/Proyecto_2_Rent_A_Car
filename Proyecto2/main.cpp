52c34cfced6df2357708efea12412fc04d371e2d
María Emilia <maria.gamboa.abarca@est.una.ac.cr>
2025-10-29 23:33:09 -0600

    ya corre yey

--- Proyecto2/main.cpp
+++ Proyecto2/main.cpp
@@ -6,12 +6,6 @@ using namespace std;
 int main() {
     controller* controlador = new controller();
 
-    cout << "========================================\n";
-    cout << "   SISTEMA D-R-T CAR RENTAL\n";
-    cout << "========================================\n";
-    cout << "\nPresione ENTER para continuar...\n";
-    cin.get();
-
     controlador->iniciarSistema();
 
     delete controlador;
